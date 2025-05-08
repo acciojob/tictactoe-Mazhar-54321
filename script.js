79a9197cf81597f6b79ba584e44361c34a2b4bca
Mazhar-54321 <160312563+Mazhar-54321@users.noreply.github.com>
2025-05-08 16:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 //your JS code here. If required.
 let player1Name="",player2Name="",currentPlayer=0 ,isWinner=false
-function onSubmit(){
-	console.log("hi")
-	let name1=document.getElementById("player-1").value;
-	let name2=document.getElementById("player-2").value;
-	console.log("hi",name1,name2);
-	player1Name=name1;
-	player2Name=name2;
-	if(name1.length && name2.length){
-		let divContainer = document.getElementsByClassName("container")[0];
-		console.log(divContainer);
-		divContainer.removeChild(document.getElementById("initialContainer"));
-		
-		let table = document.createElement("table");
-		//table
-		//table.style.border="1px solid black";
-		table.style.width="300px";
-		table.style.height="300px";
-		table.border="1";
-		table.style.borderCollapse="collapse"
-		const data=[["","",""],["","",""],["","",""]];
-		let c=0;
-		data.forEach((el,index1)=>{
-			const tr = document.createElement("tr");
-			// tr.style.border="1px solid black";
-			tr.border="2";
-			el.forEach((el1,index)=>{
-				const td = document.createElement("td");
-				td.innerText = el1;
-				td.border="2";
-				td.id=c;
-				td.style.cursor="pointer";
-				td.style.height="100px";
-				td.style.width="100px";
-				td.style.background="pink"
-				c++
-				// tr.style.border="1px solid black";
-				//document.addEventListener
-				td.addEventListener("click",()=>{
-					clickHandler(td.id);
-				})
-				tr.appendChild(td);
-			})
-			table.appendChild(tr)
-		})
-		const div = document.createElement("div");
-		div.className="message";
-		div.innerText=`${player1Name}, you're up`;
-		div.style.fontWeight="bold";
-		div.style.margin="5px"
-		divContainer.appendChild(div)
-		divContainer.appendChild(table)
-	}
+function onSubmit() {
+  console.log("hi")
+  let name1 = document.getElementById("player-1").value;
+  let name2 = document.getElementById("player-2").value;
+  console.log("hi", name1, name2);
+  player1Name = name1;
+  player2Name = name2;
+  if (name1.length && name2.length) {
+    let divContainer = document.getElementsByClassName("container")[0];
+    console.log(divContainer);
+    divContainer.removeChild(document.getElementById("initialContainer"));
+
+    let table = document.createElement("table");
+    //table
+    //table.style.border="1px solid black";
+    table.style.width = "300px";
+    table.style.height = "300px";
+    table.border = "1";
+    table.style.borderCollapse = "collapse"
+    const data = [["", "", ""], ["", "", ""], ["", "", ""]];
+    let c = 1;
+    data.forEach((el, index1) => {
+      const tr = document.createElement("tr");
+      // tr.style.border="1px solid black";
+      tr.border = "2";
+      el.forEach((el1, index) => {
+        const td = document.createElement("td");
+        td.innerText = el1;
+        td.border = "2";
+        td.id = c;
+        td.style.cursor = "pointer";
+        td.style.height = "100px";
+        td.style.width = "100px";
+        td.style.background = "pink"
+        c++
+        // tr.style.border="1px solid black";
+        //document.addEventListener
+        td.addEventListener("click", () => {
+          clickHandler(td.id);
+        })
+        tr.appendChild(td);
+      })
+      table.appendChild(tr)
+    })
+    const div = document.createElement("div");
+    div.className = "message";
+    div.innerText = `${player1Name}, you're up`;
+    div.style.fontWeight = "bold";
+    div.style.margin = "5px"
+    divContainer.appendChild(div)
+    divContainer.appendChild(table)
+  }
 }
 function clickHandler(id) {
   let td = document.getElementById(id);
@@ -62,7 +62,7 @@ function clickHandler(id) {
     } else {
       td.innerText = 'X'
     }
-    let cell0 = document.getElementById("0").innerText
+    // let cell0 = document.getElementById("0").innerText
     let cell1 = document.getElementById("1").innerText
     let cell2 = document.getElementById("2").innerText
     let cell3 = document.getElementById("3").innerText
@@ -71,8 +71,9 @@ function clickHandler(id) {
     let cell6 = document.getElementById("6").innerText
     let cell7 = document.getElementById("7").innerText
     let cell8 = document.getElementById("8").innerText;
+    let cell9 = document.getElementById("9").innerText;
     const obj = {
-      0:cell0,
+      // 0:cell0,
       1:cell1,
       2:cell2,
       3:cell3,
@@ -80,168 +81,11 @@ function clickHandler(id) {
       5:cell5,
       6:cell6,
       7:cell7,
-      8:cell8
+      8:cell8,
+      9:cell9
     }
     var xCount = 0, oCount = 0;
-    for(let i=0;i<3;i++){
-       xCount += obj[i]=== "X" ? 1 : 0;
-       oCount += obj[i]=== "O" ? 1 : 0;
-    }
-    if (xCount === 3) {
-      document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-      isWinner=true;
-      return;
-    }
-    if (oCount === 3) {
-      document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-      isWinner=true;
-      return;
-    }
-    var xCount = 0, oCount = 0;
-    for(let i=3;i<6;i++){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   var xCount = 0, oCount = 0;
-    for(let i=6;i<9;i++){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   /////
-   var xCount = 0, oCount = 0;
-    for(let i=0;i<=6;i+=3){
-       xCount += obj[i]=== "X" ? 1 : 0;
-       oCount += obj[i]=== "O" ? 1 : 0;
-    }
-	  console.log(xCount,oCount);
-    if (xCount === 3) {
-      document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-      isWinner=true;
-      return;
-    }
-    if (oCount === 3) {
-      document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-      isWinner=true;
-      return;
-    }
-    var xCount = 0, oCount = 0;
-    for(let i=1;i<=7;i+=3){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   var xCount = 0, oCount = 0;
-    for(let i=2;i<=8;i+=3){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   var xCount = 0, oCount = 0;
-    for(let i=0;i<=8;i+=4){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   var xCount = 0, oCount = 0;
-    for(let i=2;i<=6;i+=2){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     isWinner=true;
-     return;
-   }
-
- 
-
-   
-    document.getElementsByClassName("message")[0].innerText = currentPlayer ? `${player1Name},you're up` : `${player2Name},you're up`
-    currentPlayer = 1 - currentPlayer;
-  }
-}function clickHandler(id) {
-  let td = document.getElementById(id);
-  let innerText = td.innerText;
-  if (innerText === "" && !isWinner) {
-    if (currentPlayer) {
-      td.innerText = 'O'
-    } else {
-      td.innerText = 'X'
-    }
-    let cell0 = document.getElementById("0").innerText
-    let cell1 = document.getElementById("1").innerText
-    let cell2 = document.getElementById("2").innerText
-    let cell3 = document.getElementById("3").innerText
-    let cell4 = document.getElementById("4").innerText
-    let cell5 = document.getElementById("5").innerText
-    let cell6 = document.getElementById("6").innerText
-    let cell7 = document.getElementById("7").innerText
-    let cell8 = document.getElementById("8").innerText;
-    const obj = {
-      0:cell0,
-      1:cell1,
-      2:cell2,
-      3:cell3,
-      4:cell4,
-      5:cell5,
-      6:cell6,
-      7:cell7,
-      8:cell8
-    }
-    var xCount = 0, oCount = 0;
-    for(let i=0;i<3;i++){
+    for(let i=1;i<4;i++){
        xCount += obj[i]=== "X" ? 1 : 0;
        oCount += obj[i]=== "O" ? 1 : 0;
     }
@@ -258,7 +102,7 @@ function clickHandler(id) {
       return;
     }
     var xCount = 0, oCount = 0;
-    for(let i=3;i<6;i++){
+    for(let i=4;i<=6;i++){
       xCount += obj[i]=== "X" ? 1 : 0;
       oCount += obj[i]=== "O" ? 1 : 0;
    }
@@ -275,7 +119,7 @@ function clickHandler(id) {
      return;
    }
    var xCount = 0, oCount = 0;
-    for(let i=6;i<9;i++){
+    for(let i=7;i<=9;i++){
       xCount += obj[i]=== "X" ? 1 : 0;
       oCount += obj[i]=== "O" ? 1 : 0;
    }
@@ -294,7 +138,7 @@ function clickHandler(id) {
    }
    /////
    var xCount = 0, oCount = 0;
-    for(let i=0;i<=6;i+=3){
+    for(let i=1;i<=7;i+=3){
        xCount += obj[i]=== "X" ? 1 : 0;
        oCount += obj[i]=== "O" ? 1 : 0;
     }
@@ -312,29 +156,29 @@ function clickHandler(id) {
       return;
     }
     var xCount = 0, oCount = 0;
-    for(let i=1;i<=7;i+=3){
-      xCount += obj[i]=== "X" ? 1 : 0;
-      oCount += obj[i]=== "O" ? 1 : 0;
-   }
-   if (xCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
-     givebackgroundColor([1,4,7])
-     isWinner=true;
-     return;
-   }
-   if (oCount === 3) {
-     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
-     givebackgroundColor([1,4,7])
-     isWinner=true;
-     return;
-   }
-   var xCount = 0, oCount = 0;
     for(let i=2;i<=8;i+=3){
       xCount += obj[i]=== "X" ? 1 : 0;
       oCount += obj[i]=== "O" ? 1 : 0;
    }
    if (xCount === 3) {
      document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
+     givebackgroundColor([1,4,7])
+     isWinner=true;
+     return;
+   }
+   if (oCount === 3) {
+     document.getElementsByClassName("message")[0].innerText = `${player2Name}, congratulations you won!`;
+     givebackgroundColor([1,4,7])
+     isWinner=true;
+     return;
+   }
+   var xCount = 0, oCount = 0;
+    for(let i=3;i<=9;i+=3){
+      xCount += obj[i]=== "X" ? 1 : 0;
+      oCount += obj[i]=== "O" ? 1 : 0;
+   }
+   if (xCount === 3) {
+     document.getElementsByClassName("message")[0].innerText = `${player1Name}, congratulations you won!`;
      givebackgroundColor([2,5,8])
      isWinner=true;
      return;
@@ -346,7 +190,7 @@ function clickHandler(id) {
      return;
    }
    var xCount = 0, oCount = 0;
-    for(let i=0;i<=8;i+=4){
+    for(let i=1;i<=9;i+=4){
       xCount += obj[i]=== "X" ? 1 : 0;
       oCount += obj[i]=== "O" ? 1 : 0;
    }
@@ -363,7 +207,7 @@ function clickHandler(id) {
      return;
    }
    var xCount = 0, oCount = 0;
-    for(let i=2;i<=6;i+=2){
+    for(let i=3;i<=7;i+=2){
       xCount += obj[i]=== "X" ? 1 : 0;
       oCount += obj[i]=== "O" ? 1 : 0;
    }
@@ -385,8 +229,9 @@ function clickHandler(id) {
   }
 }
 function givebackgroundColor(arr){
+	console.log(arr)
   for(let i=0;i<arr.length;i++){
-    document.getElementById(i+"").style.background="purple"
+    document.getElementById((arr[i]+1)+"").style.background="purple"
   }
 }
 
